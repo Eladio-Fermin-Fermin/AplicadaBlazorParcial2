7c2d89f437f55852887e122457f0e87776b98ab0
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-03-10 19:18:55 -0400

    Se colocaron las Id en Ventas.

---
+++ AplicadaBlazorParcial2/BLL/CobrosBLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicadaBlazorParcial2.BLL
+{
+    public class CobrosBLL
+    {
+
+    }
+}
