9c558a497b4e8f92c4c67e4210ca2d09d6c71d9e
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-03-10 14:11:17 -0400

    Se creo el Contexto.

---
+++ AplicadaBlazorParcial2/DAL/Contexto.cs
@@ -0,0 +1,44 @@
+﻿using AplicadaBlazorParcial2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicadaBlazorParcial2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Clientes> Cliente { get; set; }
+        public DbSet<Ventas> Venta { get; set; }
+        public DbSet<Cobros> Cobro { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> option) : base(option)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Clientes>().HasData(new Clientes() { Nombres = "FERRETERIA GAMA" });
+
+            modelBuilder.Entity<Ventas>().HasData(new Ventas() { Fecha = new DateTime(2020, 09, 01), ClienteId = 1, Monto = 1000, Balance = 1000 });
+
+            modelBuilder.Entity<Ventas>().HasData(new Ventas() { Fecha = new DateTime(2020, 10, 01), ClienteId = 1, Monto = 900, Balance = 800 });
+
+            modelBuilder.Entity<Clientes>().HasData(new Clientes() { Nombres = "AVALON DISCO" });
+
+            modelBuilder.Entity<Ventas>().HasData(new Ventas() { Fecha = new DateTime(2020, 09, 01), ClienteId = 2, Monto = 2000, Balance = 2000 });
+
+            modelBuilder.Entity<Ventas>().HasData(new Ventas() { Fecha = new DateTime(2020, 10, 01), ClienteId = 2, Monto = 1900, Balance = 1800 });
+
+            modelBuilder.Entity<Clientes>().HasData(new Clientes() { Nombres = "PRESTAMOS CEFIPROD" });
+
+            modelBuilder.Entity<Ventas>().HasData(new Ventas() { Fecha = new DateTime(2020, 09, 01), ClienteId = 3, Monto = 3000, Balance = 3000 });
+
+            modelBuilder.Entity<Ventas>().HasData(new Ventas() { Fecha = new DateTime(2020, 10, 01), ClienteId = 3, Monto = 2900, Balance = 1900 });
+
+        }
+    }
+}
