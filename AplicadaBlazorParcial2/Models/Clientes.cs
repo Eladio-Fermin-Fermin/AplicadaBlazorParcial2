8eb28d9922afd1ca6cd5d5606a8f95257c9650af
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-03-10 14:08:35 -0400

    Se crearon las clases.

---
+++ AplicadaBlazorParcial2/Models/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicadaBlazorParcial2.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+    }
+}
