a1fc522aaa9616543f4a27bd89a1deb6f2b4b93f
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-03-10 19:04:51 -0400

    Se creo la BLL ClientesBLL y se esta programando la BLL ventas.

---
+++ AplicadaBlazorParcial2/BLL/ClientesBLL.cs
@@ -0,0 +1,141 @@
+﻿using AplicadaBlazorParcial2.DAL;
+using AplicadaBlazorParcial2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AplicadaBlazorParcial2.BLL
+{
+    public class ClientesBLL
+    {
+        private Contexto contexto { get; set; }
+        public ClientesBLL(Contexto contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        //Metodo Existe
+        public async Task<bool> Existe(int id)
+        {
+            bool encontrado = false;
+            try
+            {
+                encontrado = await contexto.Cliente.AnyAsync(c => c.ClienteId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return encontrado;
+        }
+
+        //Metodo insertar
+        public async Task<bool> Insertar(Clientes clientes)
+        {
+            bool paso = false;
+            try
+            {
+                await contexto.Cliente.AddAsync(clientes);
+                paso = await contexto.SaveChangesAsync() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+
+        }
+        //Metodo
+        private async Task<bool> Modificar(Clientes clientes)
+        {
+            bool paso = false;
+            try
+            {
+                contexto.Entry(clientes).State = EntityState.Modified;
+                paso = await contexto.SaveChangesAsync() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        //Metodo Guardar
+        public async Task<bool> Guardar(Clientes clientes)
+        {
+            if (!await Existe(clientes.ClienteId))
+                return await Insertar(clientes);
+            else
+                return await Modificar(clientes);
+        }
+
+        //Metodo Buscar.
+        public async Task<Clientes> Buscar(int id)
+        {
+            Clientes clientes;
+            try
+            {
+                clientes = await contexto.Cliente.FindAsync(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return clientes;
+        }
+
+        //Metodo Eliminar
+        public async Task<bool> Eliminar(int id)
+        {
+            bool paso = false;
+            try
+            {
+                var clientes = await contexto.Cliente.FindAsync(id);
+                if (clientes != null)
+                {
+                    contexto.Cliente.Remove(clientes);
+                    paso = await contexto.SaveChangesAsync() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        //Metodo GetList.
+        public async Task<List<Clientes>> GetClientes(Expression<Func<Clientes, bool>> criterio)
+        {
+            List<Clientes> lista = new List<Clientes>();
+            try
+            {
+                lista = await contexto.Cliente.Where(criterio).ToListAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return lista;
+        }
+        //
+        public async Task<List<Clientes>> GetClientes()
+        {
+            List<Clientes> lista = new List<Clientes>();
+            try
+            {
+                lista = await contexto.Cliente.ToListAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return lista;
+        }
+
+    }
+}
---
+++ AplicadaBlazorParcial2/BLL/VentasBLL.cs
@@ -0,0 +1,101 @@
+﻿using AplicadaBlazorParcial2.DAL;
+using AplicadaBlazorParcial2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicadaBlazorParcial2.BLL
+{
+    public class VentasBLL
+    {
+        private Contexto contexto { get; set; }
+        public VentasBLL(Contexto contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        //Metodo Existe.
+        public async Task<bool> Existe(int id)
+        {
+            bool encontrado = false;
+            try
+            {
+                encontrado = await contexto.Venta.AnyAsync(v => v.VentaId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return encontrado;
+        }
+
+        //Metodo Insertar.
+        public async Task<bool> Insertar(Ventas ventas)
+        {
+            bool paso = false;
+            try
+            {
+                await contexto.Venta.AddAsync(ventas);
+                paso = await contexto.SaveChangesAsync() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        //Metodo Modificar
+        private async Task<bool> Modificar(Ventas ventas)
+        {
+            bool paso = false;
+            try
+            {
+                contexto.Entry(ventas).State = EntityState.Modified;
+                paso = await contexto.SaveChangesAsync() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        //Metodo Guardar
+        public async Task<bool> Guardar(Ventas ventas)
+        {
+            if (!await Existe(ventas.VentaId))
+                return await Insertar(ventas);
+            else
+                return await Modificar(ventas);
+        }
+
+        //Metodo Buscar
+        public async Task<Ventas> Buscar(int id)
+        {
+            Ventas ventas;
+            try
+            {
+                ventas = await contexto.Venta.FindAsync(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return ventas;
+        }
+
+        //Metodo Eliminar
+        public async Task<bool> Eliminar(int id)
+        {
+            try
+            {
+                var ventas = await contexto.Venta.FindAsync(id);
+
+            }
+        }
+
+    }
+}
